7cf77c5afe183a3524ee73f1e779fef2692529b6
Tobias Quante <t.quante@outlook.com>
2020-11-23 21:42:31 +0100

    Added cors headers

--- index.js
+++ index.js
@@ -22,6 +22,14 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin, X-Requested-With, Content-Type, Accept'
+	);
+	next();
+});
 
 // Load specific middleware for dev environment
 if (process.env.NODE_ENV === 'development') {
