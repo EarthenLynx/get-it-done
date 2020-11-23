51361ecb1cb5ce478ee78999b9b59784ce92aebc
Tobias Quante <t.quante@outlook.com>
2020-11-23 21:50:56 +0100

    Added missing header for methods

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use( (req, res, next) => {
+	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 	res.header('Access-Control-Allow-Origin', '*');
 	res.header(
 		'Access-Control-Allow-Headers',
