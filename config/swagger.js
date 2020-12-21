f491892639a9e875162f8d7fb126ec96de3420f1
Tobias Quante <t.quante@outlook.com>
2020-12-21 12:53:14 +0100

    Adjusted swagger spec

--- config/swagger.js
+++ config/swagger.js
@@ -12,19 +12,19 @@ const swaggerSpecs = {
 	definition: {
 		openapi: '3.0.0',
 		info: {
-			title: 'Express API Swagger boilerplate',
-			version: '1.2.3',
+			title: 'Get it done REST API service',
+			version: '1.0.0',
 			description:
-				'This is a boilerplate to document openapi interfaces. Please refrain to the MD file in the root file for more info',
+				'Get it done API provides an interface to connect to your workspace from other apps. The application\'s approach follows a workflow that embraces the "Keep it simple, stupid" paradigm, while offering enough space to customize actions, if necessary',
 			license: {
 				name: 'MIT',
 				url: 'https://spdx.org/licenses/MIT.html',
 			},
 			termsOfService: `${host}/terms`,
 			contact: {
-				name: 'API Admin',
+				name: 'Tobias Quante',
 				url: `${host}`,
-				email: 'info@openapi.com',
+				email: 'tobi@q-bit.me',
 			},
 		},
 		host: `${host}`,
@@ -36,33 +36,68 @@ const swaggerSpecs = {
 	},
 	tags: [
 		{
-			name: 'Users',
-			description: 'API endpoint to handle user data',
+			name: 'Intray',
+			description:
+				'Primary resource: An intray item is the equivalent to the later-action. It reaches the end of its lifecycle as soon as it is classified by a user to one of the other resource types.',
 		},
 		{
-			name: 'Todos',
-			description: 'API endpoint to handle todo data',
+			name: 'Information',
+			description:
+				'Primary resource: Informaiton items are not related to any action or appointment, but include knowledge for a later usage.',
+		},
+		{
+			name: 'Waiting for',
+			description: 'Primary resource: Delegated tasks, in case oneself is not the right person for the job.',
+		},
+		{
+			name: 'Calender',
+			description:
+				'Primary resource: Tasks or actions that can be related to a single point or interval of time in the future.',
+		},
+		{
+			name: 'Next action',
+			description: 'Primary resource: Tasks or actions that should be done next.',
+		},
+		{
+			name: 'Project',
+			description:
+				'Primary resource: Projects are a collection of next action - items. Project can keep a backlog of the other items.',
+		},
+		{
+			name: 'Action lists',
+			description:
+				'Secondary resource: Instead of putting them into a single todo - list, action items can be placed into a single list. These can be customized by a user.',
+		},
+		{
+			name: 'Operators',
+			description: 'People to which actions or tasks can be delegated',
+		},
+		{
+			name: 'Users',
+			description:
+				'Secondary resource: Users can perform operations on the API. Also, this resource is used to customize the UI or user settings',
+		},
+		{
+			name: 'Auth',
+			description:
+				'Secondary resource: The auth resource is used to authenticate users against a single endpoint. The primary keys to access these have to be the same as for the user resource',
+		},
+		{
+			name: 'Logs',
+			description:
+				'Secondary resource: Each task can include several logs. These might be single subtasks that have been done, communication that happened, etc.',
+		},
+		{
+			name: 'Clients',
+			description:
+				'Secondary resource: A client is somebody to do work for. This resource includes their contact- and payment information.',
+		},
+		{
+			name: 'Efforts',
+			description:
+				'Secondary resource: Efforts log the time being spent on a single task. They also link the action to a client.',
 		},
 	],
-	components: {
-		schemas: {
-			BadRequest: {
-				type: 'object',
-				properties: {
-					status: {
-						type: 'string',
-						description:
-							'Describes the type of error that occured during execution',
-					},
-					msg: {
-						type: 'string',
-						description:
-							'Includes a message for the user to see what went wrong',
-					},
-				},
-			},
-		},
-	},
 	apis: ['./api/v1/routes/user.yaml'],
 };
 
