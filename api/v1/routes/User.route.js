9d9f3115a326b0f78b8ed7f2211e3642e6abe51b
Tobias Quante <t.quante@outlook.com>
2021-02-17 18:10:35 +0100

    added thisUser middleware

--- api/v1/routes/User.route.js
+++ api/v1/routes/User.route.js
@@ -1,21 +1,25 @@
 const router = require('express').Router();
 const UserController = require('../../../controller/User.controller');
-const { verifyUser, verifyGuest } = require('../../../middleware/verifyUser');
+const {
+  verifyUser,
+  verifyGuest,
+  authorizeThisUser,
+} = require('../../../middleware/verifyUser');
 const { noEmptyPayload } = require('../../../middleware/verifyCommunication');
 
 router.get('/', verifyGuest, (req, res) => {
   UserController.handleGetUsers(req, res);
 });
 
-router.get('/:userId', verifyGuest, (req, res) => {
+router.get('/:userId', verifyGuest, authorizeThisUser, (req, res) => {
   UserController.handleGetUserByUserId(req, res);
 });
 
-router.put('/:userId', verifyGuest, noEmptyPayload, (req, res) => {
+router.put('/:userId', verifyGuest, authorizeThisUser, noEmptyPayload, (req, res) => {
   UserController.handleUpdateUserByUserId(req, res);
 });
 
-router.delete('/:userId', verifyGuest, (req, res) => {
+router.delete('/:userId', verifyGuest, authorizeThisUser, (req, res) => {
   UserController.handleDeleteUserByUserId(req, res);
 });
 
